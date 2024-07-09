1f579fd0142e14ac347716fae5b6e803e2aa7036
Alexander Gorlenko <60706892+Executioner34@users.noreply.github.com>
2024-07-09 14:49:01 +0300

    feat: base config frontend

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,5 +1,37 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: process.env.NODE_ENV !== 'production' },
+  srcDir: 'src/',
+  dir: {
+    public: 'app/public',
+    layouts: 'app/layouts',
+    assets: 'app/assets',
+    middleware: 'app/middleware',
+    plugins: 'app/plugins',
+  },
+  components: [
+    {
+      path: '~/shared/ui',
+      pathPrefix: false,
+    },
+  ],
+  modules: ['@nuxt/eslint', '@pinia/nuxt', '@vueuse/nuxt'],
+  eslint: {
+    config: {
+      stylistic: true,
+    },
+  },
+  css: ['normalize.css/normalize.css', '@/app/assets/scss/main.scss'],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+            @use "@/app/assets/scss/_colors.scss" as *;
+            @use "@/app/assets/scss/_mixins.scss" as *;
+          `,
+        },
+      },
+    },
+  },
 })
